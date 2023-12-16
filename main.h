f4323a4ca9c5650845fc511f3328547d457bb138
innobel <innocentabel11@gmail.com>
2023-12-15 20:35:20 -0800

    header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void handle_command(char *input);
+
+#endif /* MAIN_H */
